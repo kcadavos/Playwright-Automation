797b3b748bb35846cb1d6c5e122c0a0cb037bdac
espressotoexpressions <keannevillar@gmail.com>
2026-08-22 19:23:39 -0700

    create util file

--- tests/WebAPIPart1.spec.js
+++ tests/WebAPIPart1.spec.js
@@ -1,6 +1,8 @@
+
 const {test,expect,request}=require("@playwright/test");
+const {APIUtils} = require('./utils/APIUtils');
+
 const loginPayload = {userEmail: "ksmith@gmail.com", userPassword: "12345Pass"}
-let token; 
 const orderPayload={
     orders: [
         {
@@ -9,39 +11,18 @@ const orderPayload={
         }
     ]
 };
-let orderId;
+let response;
 
 
 //will be executed before any other test
 test.beforeAll(async ()=>{
 
-   const apiContext =  await request.newContext();
-
-   //login api call
-   const loginResponse= await apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login",
-    {
-        data:loginPayload
-    });
-
-    expect(loginResponse.ok()).toBeTruthy();// will check if status code returned is 200
-    const loginResponseJSON= await loginResponse.json()
-     token =  loginResponseJSON.token;// extracting token
-
-     console.log("TOKEN:"+ token);
+    const apiContext =  await request.newContext();
+    const apiUtils = new APIUtils(apiContext, loginPayload);
+    response = await apiUtils.createOrder(orderPayload);
 
      // order API CALL
-     const orderResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/order/create-order",
-     {
-        data:orderPayload,
-        headers:{
-            'Authorization' :token,
-            'Content-Type' : 'application/json',
-              },
-     })
-
-     const orderResponseJSON = await orderResponse.json();
-    orderId = orderResponseJSON.orders[0];
-     console.log ("ORDERID FROM API:"+ orderId);
+     
 
 });//end of beforeAll
 
@@ -51,11 +32,13 @@ test.beforeEach (()=>{
 })
 
 test("Add Items to Cart", async ({page})=> {
-    // skip login using api call instead of UI
+
+    // skip login and order placement using api call instead of UI
     page.addInitScript(value =>{ // value to pass
         window.localStorage.setItem('token',value);
 
-    },token)
+    },response.token)
+
     await page.goto("https://rahulshettyacademy.com/client/");
     // login api and order placement api are called 
 
@@ -70,7 +53,7 @@ test("Add Items to Cart", async ({page})=> {
                 
                    const rowElement =  page.locator("tbody .ng-star-inserted").nth(i);
                    const rowOrderText = await rowElement.locator("[scope*='row']").textContent();
-                     if(rowOrderText.includes(orderId))
+                     if(rowOrderText.includes(response.orderId))
                      {
                         console.log("row order text " + rowOrderText);
                          await (rowElement.locator(".btn-primary")).click();
@@ -84,7 +67,7 @@ test("Add Items to Cart", async ({page})=> {
         const summaryOrderIdText =await summaryOrderId.textContent();
         console.log("SUMMARY ORDER TEXT: "+ summaryOrderIdText)
         // await expect (orderId from API).toHaveText(cleanOrderText);
-        expect  (await orderId.includes(summaryOrderIdText)).toBeTruthy();
+        expect  (await response.orderId.includes(summaryOrderIdText)).toBeTruthy();
 
          await page.pause();
     });
