07bfeff079b50034fcf04ecc670801c1b1f65cb6
espressotoexpressions <keannevillar@gmail.com>
2026-08-22 19:55:40 -0700

    Add state json file

---
+++ tests/WebApiPart2.spec.js
@@ -0,0 +1,34 @@
+const {test,expect} = require('@playwright/test');
+let webContext;
+
+test.beforeAll(async({browser})=>{
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://rahulshettyacademy.com/client/");
+    const email = page.locator ('#userEmail');
+    const emailValue = "ksmith@gmail.com";
+    await email.fill(emailValue);
+
+    const password = page.locator ('#userPassword');
+    await password.fill ('12345Pass');
+
+    const loginBtn = page.locator ('#login');
+    await loginBtn.click();
+    await page.waitForLoadState('networkidle');
+    await context.storageState({path:'state.json'});
+    webContext =  await browser.newContext({storageState:'state.json'});
+
+})
+
+test("Login", async ({})=> {
+
+   
+    const page = await webContext.newPage();
+    await page.goto("https://rahulshettyacademy.com/client/");
+    const allCardBodyTitles = page.locator('.card-body b');  // parent child locator
+    await allCardBodyTitles.first().waitFor(); // another sync step for all textContents returning an array
+
+
+    console.log (await allCardBodyTitles.allTextContents()); 
+
+    });
