87065fe734539d090ae88aea7f981dc7e45a0c5f
espressotoexpressions <keannevillar@gmail.com>
2026-08-25 18:37:48 -0700

    add iteration of cels inside the sheet

---
+++ tests/excelDemo.js
@@ -0,0 +1,44 @@
+const ExcelJs= require('exceljs');
+
+async function excelTest()
+{
+
+    const workbook = new ExcelJs.Workbook();
+    
+    await  workbook.xlsx.readFile("/Users/karen/Desktop/Learning/Playwright Automation/download.xlsx");
+    
+    
+    const worksheet1 = workbook.getWorksheet('Sheet1');
+
+    let output={
+        row:-1,
+        column:-1
+    }; //output object to hold cell location to change value
+
+    //read sheet
+     worksheet1.eachRow((row, rowNumber)=>{
+        row.eachCell((cell,colNumber)=>{
+            // console.log("ROW:"+ rowNumber + " COL:" + colNumber + " VAL: "+ cell.value)
+            if (cell.value ==="Apple")
+                {
+                    // console.log("ROW#: "+rowNumber);
+                    // console.log("COL#: "+ colNumber);
+                    
+                    //update the locator if the text is matched
+                    output.row = rowNumber;
+                    output.column = colNumber;
+                }
+
+        })
+    })
+    
+   //read and write cell
+    const cell = worksheet1.getCell(output.row,output.column);
+    cell.value = "Iphone";
+    await workbook.xlsx.writeFile("/Users/karen/Desktop/Learning/Playwright Automation/download.xlsx");
+
+
+}
+
+
+excelTest();
